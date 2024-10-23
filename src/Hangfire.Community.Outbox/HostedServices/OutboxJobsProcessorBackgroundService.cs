2c33f703ea7980f132730ea2d21ddb12f08f4015
BredStik <mathieulafontaine@hotmail.com>
2024-10-22 20:57:41 -0400

    Added distributed lock support in outbox processor hosted service

--- src/Hangfire.Community.Outbox/HostedServices/OutboxJobsProcessorBackgroundService.cs
+++ src/Hangfire.Community.Outbox/HostedServices/OutboxJobsProcessorBackgroundService.cs
@@ -1,26 +1,63 @@
 ﻿using Hangfire.Community.Outbox.Extensions;
 using Hangfire.Community.Outbox.Services;
+using Hangfire.Storage;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Hangfire.Community.Outbox.HostedServices;
 
 public class OutboxJobsProcessorBackgroundService: BackgroundService
 {
     private readonly IOutboxProcessor _outboxProcessor;
+    private readonly JobStorage _jobStorage;
     private readonly HangfireOutboxOptions _outboxOptions;
+    private readonly ILogger<OutboxJobsProcessorBackgroundService> _logger;
 
-    public OutboxJobsProcessorBackgroundService(IOutboxProcessor outboxProcessor, HangfireOutboxOptions outboxOptions)
+    public OutboxJobsProcessorBackgroundService(IOutboxProcessor outboxProcessor, JobStorage jobStorage, HangfireOutboxOptions outboxOptions, ILogger<OutboxJobsProcessorBackgroundService> logger)
     {
         _outboxProcessor = outboxProcessor;
+        _jobStorage = jobStorage;
         _outboxOptions = outboxOptions;
+        _logger = logger;
     }
     
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        IDisposable distributedLock = null;
+        var distributedLockSupported = _jobStorage.HasFeature(JobStorageFeatures.Transaction.AcquireDistributedLock);
+
+        _logger.LogDebug("DistributedLock supported on Hangfire job storage: {enabled}", distributedLockSupported);
+
         while (!stoppingToken.IsCancellationRequested)
         {
-            await ProcessOutboxMessages(stoppingToken);    
-            await Task.Delay(_outboxOptions.OutboxProcessorFrequency, stoppingToken);
+            if (distributedLockSupported)
+            {
+                try
+                {
+                    _logger.LogDebug("Trying to obtain distributed lock");
+                    distributedLock = _jobStorage.GetConnection().AcquireDistributedLock(nameof(OutboxJobsProcessorBackgroundService), TimeSpan.FromSeconds(10));
+                    _logger.LogDebug("Obtained distributed lock");
+                }
+                catch (Exception exc)
+                {
+                    _logger.LogError(exc, "Could not obtain distributed lock");
+                    //could not obtain lock
+                }
+
+                if (distributedLock == null && distributedLockSupported)
+                {
+                    _logger.LogDebug("Waiting before trying to update a distributed lock again");
+                    await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+                    continue;
+                }
+            }
+            
+
+            await ProcessOutboxMessages(stoppingToken);
+
+            distributedLock!.Dispose();
+
+            await Task.Delay(_outboxOptions.OutboxProcessorFrequency, stoppingToken);            
         }
     }
 
